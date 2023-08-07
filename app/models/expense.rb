d23e1a9fa13cc5797147e57c467ed5bcd91ff8c7
christianonoh <onohchibyk@gmail.com>
2023-08-07 23:22:36 +0100

    Modify expense model to validate the parameters

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :author_id, presence: true
 end
