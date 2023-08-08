a04ff0e8cee16a09de59f317f5b1466d5f8edabd
christianonoh <onohchibyk@gmail.com>
2023-08-08 12:05:42 +0100

    Update associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: "User"
+  has_many :category_expenses
+  has_many :expenses, through: :category_expenses
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :icon, presence: true
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :category_expenses
+  has_many :categories, through: :category_expenses
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
