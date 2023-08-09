e5fd9be0acbeb0c21fb9e54b146f83353df683ed
christianonoh <onohchibyk@gmail.com>
2023-08-09 15:31:22 +0100

    Fix linters

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'sassc-rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
@@ -77,7 +78,6 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
-  gem 'rspec-rails'
 end
 
 gem 'devise', '~> 4.9'
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new  # Assuming User is your user model class
+    user ||= User.new # Assuming User is your user model class
 
     # Only logged in users can manage categories and expenses
-    if user.id.present?
-      can :manage, Category, author_id: user.id
-      can :manage, Expense, author_id: user.id
-    end
+    return unless user.id.present?
+
+    can :manage, Category, author_id: user.id
+    can :manage, Expense, author_id: user.id
   end
 end
