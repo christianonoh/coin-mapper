14892f7d990cb0bc8d5f93668a0303dc5c6175ff
christianonoh <onohchibyk@gmail.com>
2023-08-10 02:44:19 +0100

    Add desktop version

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :expense_categories
+  has_many :expense_categories, dependent: :destroy
   has_many :expenses, through: :expense_categories
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :expense_categories
+  has_many :expense_categories, dependent: :destroy
   has_many :categories, through: :expense_categories
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
